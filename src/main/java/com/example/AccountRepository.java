ccf957da1f498a6afbd6d1e346d0edbe0b613962
Kristina M <kristina0x7@gmail.com>
2025-12-04 14:17:31 +0100

    Impl AccountRepository interface

--- src/main/java/com/example/AccountRepository.java
+++ src/main/java/com/example/AccountRepository.java
@@ -1,4 +1,12 @@
 package com.example;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AccountRepository {
+    Optional<Account> findNameAndPassword(String name, String password);
+    List<Account> findAll();
+    long create(Account account);
+    boolean updatePassword(long userId, String newPassword);
+    boolean delete(long userId);
 }
