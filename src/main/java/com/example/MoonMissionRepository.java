ebb32093dc2a3ea684af08a180dc16af55c98937
Kristina M <kristina0x7@gmail.com>
2025-12-04 14:17:52 +0100

    Impl MoonMissionRepository interface

--- src/main/java/com/example/MoonMissionRepository.java
+++ src/main/java/com/example/MoonMissionRepository.java
@@ -1,4 +1,12 @@
 package com.example;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface MoonMissionRepository {
+    List<MoonMission> findAll();
+    Optional<MoonMission> findById(long missionId);
+    long create(MoonMission mission);
+    boolean updateSpacecraft(long missionId, String newSpacecraft);
+    boolean delete(long missionId);
 }
