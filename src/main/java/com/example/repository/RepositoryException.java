6ddbfe30ffe9723bdb7b7ffb9b06c37a201e806a
Kristina M <kristina0x7@gmail.com>
2025-12-11 12:00:22 +0100

    add class RepositoryException to repository package

---
+++ src/main/java/com/example/repository/RepositoryException.java
@@ -0,0 +1,7 @@
+package com.example.repository;
+
+public class RepositoryException extends RuntimeException {
+    public RepositoryException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
